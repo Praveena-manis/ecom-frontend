a62a886425811e97bcf899147cc75f5b18da2782
Praveena-manis <143096344+Praveena-manis@users.noreply.github.com>
2024-01-26 23:27:44 +0530

    Delete node.js directory

--- node.js/ecommerce/backend/config/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');//used mongoose for connection and used dotenv for configuration
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("connected"))
-.catch((error)=>console.log("error in connection:"+error))
--- node.js/ecommerce/backend/models/category_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const categorySchema=new mongoose.Schema({
-//used category schema for category
-name:{
-    type:String,
-}
-},{timestamps:true});
-const categoryModel=mongoose.model("categoryModel",categorySchema);
-module.exports=categoryModel;
